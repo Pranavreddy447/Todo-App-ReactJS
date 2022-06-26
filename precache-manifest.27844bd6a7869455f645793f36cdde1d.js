8f61dcc987af19137ef68c5603ae6c0661c713b7
Pranav Reddy Gunnala <pranavreddygunnala555@gmail.com>
2022-06-27 01:21:32 +0530

    Updates

---
+++ precache-manifest.27844bd6a7869455f645793f36cdde1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "412a0d40fcc7a4469f48",
+    "url": "/Todo-App-ReactJS/static/css/main.36da0df2.chunk.css"
+  },
+  {
+    "revision": "412a0d40fcc7a4469f48",
+    "url": "/Todo-App-ReactJS/static/js/main.412a0d40.chunk.js"
+  },
+  {
+    "revision": "9863931913954e89bf2e",
+    "url": "/Todo-App-ReactJS/static/js/1.98639319.chunk.js"
+  },
+  {
+    "revision": "199af2c2a0c941a3b7e7",
+    "url": "/Todo-App-ReactJS/static/js/2.199af2c2.chunk.js"
+  },
+  {
+    "revision": "f72fa4f9e8ad57d7e38d",
+    "url": "/Todo-App-ReactJS/static/js/runtime~main.f72fa4f9.js"
+  },
+  {
+    "revision": "99d5c08007f790b53a9e0601d1aa87f1",
+    "url": "/Todo-App-ReactJS/index.html"
+  }
+];
